86fba4b91ee590ddaeeb9615e36f5415d85c2be4
NerdySouth <46800978+NerdySouth@users.noreply.github.com>
2024-05-16 20:10:57 -0700

    Implement packet.rs using etherparse crate and some custom MacAddr structs

--- src/probe_modules/packet.rs
+++ src/probe_modules/packet.rs
@@ -1,38 +1,45 @@
-fn make_eth_header(ethh: &mut EthHdr, src: &MacAddr, dst: &MacAddr) {
-    ethh.h_source.copy_from_slice(src);
-    ethh.h_dest.copy_from_slice(dst);
-    ethh.h_proto = htons(ETH_P_IP);
+
+#[derive(Debug, Clone)]
+pub struct PacketError;
+
+impl fmt::Display for PacketError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "[Packet Error]")
+    }
 }
 
-fn make_ip_header(iph: &mut IpHdr, protocol: u8, len: u16) {
-    iph.ihl = 5; // Internet Header Length
-    iph.version = 4; // IPv4
-    iph.tos = 0; // Type of Service
-    iph.tot_len = len;
-    iph.id = htons(54321); // identification number
-    iph.frag_off = 0; // fragmentation falg
-    iph.ttl = MAXTTL; // time to live (TTL)
-    iph.protocol = protocol; // upper layer protocol => TCP
-                             // we set the checksum = 0 for now because that's
-                             // what it needs to be when we run the IP checksum
-    iph.check = 0;
+pub struct MacAddr(eui48::MacAddr);
+
+pub impl MacAddr {
+    pub fn new(bytes: [u8; 6]) -> Self {
+         { MacAddr(eui48::MacAddr::new(bytes) }
+    }
+
+    pub fn from_bytes(bytes: &[u8; 6]) -> Self {
+        { MacAddr(eui48::MacAddr::from_bytes(bytes).unwrap()) }
+    }
+    
+    pub fn octets(&self) -> [u8; 6] {
+        self.0.to_array()
+    }
+
+    pub fn as_bytes(&self) -> &[u8] {
+        self.0.as_bytes()
+    }
+
+    pub fn parse_str(s: &str) -> Result<Self, PacketErr> {
+        match eui48::MacAddress::parse_str(s) {
+            Ok(addr) => Ok(Self(addr)),
+            Err(e) => Err("{} failed to parse MAC Address", e),
+        }
+    }
 }
 
-fn make_tcp_header(tcp_header: &mut TcpHdr, dest_port: u16) {
-    tcp_header.seq = random();
-    tcp_header.ack_seq = 0;
-    tcp_header.res2 = 0;
-    tcp_header.doff = 5; // data offset
-    tcp_header.syn = 1;
-    tcp_header.window = htons(65535); // largest possible window
-    tcp_header.check = 0;
-    tcp_header.urg_ptr = 0;
-    tcp_header.dest = htons(dest_port);
-}
 
-fn make_udp_header(udp_header: &mut UdpHdr, dest_port: u16, len: u16) {
-    udp_header.dest = htons(dest_port);
-    udp_header.len = htons(len);
-    // checksum ignored in IPv4 if 0
-    udp_header.check = 0;
-}
+
+pub struct EthHdr(etherparse::Ethernet2Header);
+pub struct IpHdr(etherparse::Ipv4Header);
+pub struct IcmpHdr(etherparse::Icmpv4Header);
+pub struct TcpHdr(etherparse::TcpHeader);
+pub struct UdpHdr(etherparse::UdpHeader);
+
